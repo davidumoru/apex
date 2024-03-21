d8b674c7758ed7f49af0545169fcec8e319451aa
David Umoru <umorudavido@gmail.com>
2024-03-21 02:24:58 +0100

    Add full charset

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,6 +1,6 @@
 export function generateId(length: number = 5): string {
   const chars =
-    "ABCDEFGHIJabcdefghij0123456789";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = length; i > 0; --i) {
     result += chars[Math.floor(Math.random() * chars.length)];

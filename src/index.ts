10f2e20b29673bcfb5f0ed384a0dcdbabcb46ffc
David Umoru <umorudavido@gmail.com>
2024-03-21 02:19:39 +0100

    Create endpoint to input repo url

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+import { generateId } from "./utils/generateId";
 
 const app: Express = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
-app.get('/', (req: Request, res: Response) => {
-  res.send('Live from the server!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Live from the server!");
+});
+
+app.post("/deploy", async (req: Request, res: Response) => {
+  const repoUrl = req.body.repoUrl;
+  const id = generateId();
+  res.json({});
 });
 
 app.listen(port, () => {
